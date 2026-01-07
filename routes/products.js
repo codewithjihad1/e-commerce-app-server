895122f84c40ca94bfd0e73ef716df806237fe05
MD Jihad Hossain <mdjihadhossain793@gmail.com>
2026-01-07 10:51:44 +0600

    feat: remove axios and oauth files, integrate WooCommerce API for product fetching

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const axiosInstance = require('../lib/axiosInstance');
-const oauth = require('../lib/oauth');
-const { default: axios } = require('axios');
+const wpApi = require('../lib/wpApiConfig');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    const resData = {
+        success: false,
+        products: [],
+    };
+
     try {
-        const requestData = {
-            url: 'http://e-commerce.local/wp-json/wc/v3/products',
-            method: 'GET',
-        };
-        const headers = oauth.toHeader(oauth.authorize(requestData));
-        const response = await axios.get(requestData.url, { headers });
-        res.json(response.data);
+        const { data } = await wpApi.get('products', { per_page: 10 });
+        resData.success = true;
+        resData.products = data;
+
+        res.status(200).send(resData);
     } catch (error) {
         console.error(error.response?.data || error.message);
         res.status(500).json(error.response?.data || error.message);
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const paymentRoute = require('./routes/payment');
-const oauth = require('./lib/oauth');
-const { default: axios } = require('axios');
 const productRoute = require('./routes/products');
 
 const app = express();
@@ -13,21 +11,6 @@ app.use(express.json());
 
 app.use('/api/payment', paymentRoute);
 app.use('/api/products', productRoute);
-
-// app.get('/products', async (req, res) => {
-//     try {
-//         const requestData = {
-//             url: 'http://e-commerce.local/wp-json/wc/v3/products',
-//             method: 'GET',
-//         };
-//         const headers = oauth.toHeader(oauth.authorize(requestData));
-//         const response = await axios.get(requestData.url, { headers });
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error(error.response?.data || error.message);
-//         res.status(500).json(error.response?.data || error.message);
-//     }
-// });
 
 app.get('/', (req, res) => {
     res.send('Server is running');
